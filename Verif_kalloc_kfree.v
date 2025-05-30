d2d23da69d45c3951eaef303e032f4b3bfdc90bb
Line Dejgaard <linedejgaard@gmail.com>
2025-05-30 08:02:32 +0200

    simplify kfree and kalloc proofs

--- Verif_kalloc_kfree.v
+++ Verif_kalloc_kfree.v
@@ -14,15 +14,15 @@ Local Open Scope logic.
 Lemma body_kfree: semax_body KAFVprog KAFGprog f_kfree (kfree_spec' KAF_APD _kfree).
 Proof. start_function.
 destruct (eq_dec new_head nullval).
-- forward_if.
-    * forward.
-    *  rewrite e in H0; auto_contradict.
-- forward_if.
-    * forward. 
-    * rewrite mem_mgr_unfold. Intros. forward. rewrite kalloc_token_sz_unfold. Intros. 
+- forward_if; auto_contradict. forward.
+- forward_if; auto_contradict.
+    forward.
+    rewrite mem_mgr_unfold. Intros. forward. 
+    rewrite kalloc_token_sz_unfold. Intros. 
     unfold t_run_size. rewrite memory_block_data_at_; auto. 
-    rewrite data_at__eq. forward. forward. forward. 
-    entailer. rewrite mem_mgr_unfold. refold_freelistrep. entailer!.
+    rewrite data_at__eq. forward. forward.
+    entailer.
+    rewrite mem_mgr_unfold. refold_freelistrep. entailer!.
     right; split; auto. unfold not; auto_contradict.
     unfold t_run_size. entailer.
 Qed.
@@ -48,22 +48,21 @@ forward_if (
           )
         )
     )
-)%assert.
-- destruct H1 as [[H0011 H0012] | [H0021 H0022]].
-    + rewrite H0012 in H2; auto_contradict.
-    + destruct ls; auto_contradict.
-        refold_freelistrep. Intros.
-        forward. forward.
-        if_tac.
-        * entailer.
-        * Exists v ls. entailer. 
-            rewrite mem_mgr_unfold. entailer!.
-            -- destruct ls.
-                ++ left; split; auto. rewrite <- H11; auto.
-                ++ right. split; unfold not; intros; auto_contradict.
-                rewrite <- H12. auto_contradict.
-            -- rewrite kalloc_token_sz_unfold. entailer!. safe_to_store_PGSIZE H3 orig_head. 
-                apply data_at_memory_block.
+).
+- destruct H1 as [[H0011 H0012] | [H0021 H0022]]; [ rewrite H0012 in H2; auto_contradict|].
+  destruct ls; auto_contradict.
+  refold_freelistrep. Intros.
+  forward. forward.
+  if_tac.
+  + entailer.
+  + Exists v ls. entailer. 
+    rewrite mem_mgr_unfold. entailer!.
+    * destruct ls.
+    -- left; split; auto. rewrite <- H11; auto.
+    -- right. split; unfold not; intros; auto_contradict.
+       rewrite <- H12. auto_contradict.
+    * rewrite kalloc_token_sz_unfold. entailer!. safe_to_store_PGSIZE H3 orig_head. 
+      apply data_at_memory_block.
 - forward. if_tac_auto_contradict. entailer. rewrite mem_mgr_unfold. entailer.
 - if_tac; forward.
 Qed.
